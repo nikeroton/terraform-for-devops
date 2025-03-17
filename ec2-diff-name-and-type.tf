a414a16c3db056030474668d83626a3ce9c59c03
Nikhil Rana <104210440+nikeroton@users.noreply.github.com>
2025-03-17 18:21:56 +0530

    Create ec2-diff-name-and-type.tf
    this is used when we need different name and type instance

---
+++ ec2-diff-name-and-type.tf
@@ -0,0 +1,74 @@
+#key pair
+
+resource "aws_key_pair" "my-key" {
+  key_name   = "terra-key"
+  public_key = file("terra-key.pub")
+}
+# VPC and security
+resource "aws_default_vpc" "default" {
+}
+resource "aws_security_group" "my-security" {
+    name        = "terra-sg"
+    description = "Allow TLS inbound traffic and all outbound traffic using terraform"
+    vpc_id      = aws_default_vpc.default.id # interpollation
+
+
+  # inbound rules
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "ssh open"
+  }
+
+  ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "http open"
+  }
+
+  ingress {
+      from_port   = 8000
+      to_port     = 8000
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "ssh open"
+  }
+
+  # outbound rules
+  egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "all access"
+  }
+
+
+  tags = {
+     Name = "terra-sg"
+  }
+}
+#ec2 instance
+resource "aws_instance" "my-instance" {
+  for_each = tomap({
+    apna-server-micro:"t2.micro"
+    apna-server-nano:"t2.nano"
+  })
+    key_name = aws_key_pair.my-key.key_name
+    security_groups = [aws_security_group.my-security.name]
+    instance_type           = each.value
+    ami                     = var.ec2_ami_id
+
+  root_block_device {
+      volume_size = var.root_volume_size
+      volume_type = "gp3"
+  }
+  tags = {
+    Name = each.key
+  }
+ 
+}
