ecdbfd7eeb565286bb989bf3c10bc9f77e63a0f9
Nikhil Rana <104210440+nikeroton@users.noreply.github.com>
2025-03-17 18:07:20 +0530

    Update outputs.tf
    added output block for single and multiple instances or resources

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,27 @@
-output "arn" {
-  value = aws_instance.testinstance.arn
+# for single instnce
+
+# output "ec2_public_ip" {
+#   value = aws_instance.my-instance.public_ip
+# }
+
+# output "ec2_private_ip" {
+#   value = aws_instance.my-instance.private_ip
+# }
+
+# output "ec2_public_dns" {
+#   value = aws_instance.my-instance.public_dns
+# }
+
+#for multiple instance
+
+output "ec2_public_ip" {
+  value = aws_instance.my-instance[*].public_ip
 }
 
-output "public_ip" {
-  value = aws_instance.testinstance.public_ip
+output "ec2_private_ip" {
+  value = aws_instance.my-instance[*].private_ip
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.my-instance[*].public_dns
 }
