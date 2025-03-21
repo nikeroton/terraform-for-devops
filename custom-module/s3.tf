017b967daf1fd5895c2261c6680aba40427d0fa4
Nikhil Rana <104210440+nikeroton@users.noreply.github.com>
2025-03-21 14:35:00 +0530

    Update and rename s3.tf to main.tf

--- custom-module/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "my-bucket-new-1asdasdad" {
-  bucket = "${var.env}-${var.bucket_name}"
-
-  tags = {
-    Name= "${var.env}-${var.bucket_name}"
-    Enviroment = var.env
-  }
-}
